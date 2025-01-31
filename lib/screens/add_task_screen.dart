8f055d90b77f81d13a87d0f93e596862004f7f1c
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-01-31 19:14:50 -0300

    Enhance AddTaskScreen with description and additional task attributes

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -5,15 +5,34 @@ import '../models/task.dart';
 
 class AddTaskScreen extends StatelessWidget {
   final TextEditingController _taskController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final DateTime _creationDate = DateTime.now();
+  final DateTime _dueDate = DateTime.now();
+  final String _priority = 'Low';
+  final String _status = 'Pending';
+  final String _updatedAt = DateTime.now().toString();
+
 
   void _addTask(BuildContext context) {
     final taskText = _taskController.text;
+    final descriptionText = _descriptionController.text;
+    final creationDate = _creationDate.toString();
+    final dueDate = _dueDate.toString();
+    final priority = _priority;
+    final status = _status;
+    final updatedAt = _updatedAt;
 
     if (taskText.isNotEmpty) {
       Provider.of<TaskProvider>(context, listen: false).addTask(
         Task(
           id: DateTime.now().toString(),
           title: taskText,
+          description: descriptionText,
+          creationDate: creationDate,
+          dueDate: dueDate,
+          priority: priority,
+          status: status,
+          updatedAt: updatedAt,
           isDone: false,
         )
       );
@@ -37,10 +56,16 @@ class AddTaskScreen extends StatelessWidget {
               decoration: InputDecoration(labelText: 'Task'),
             ),
             SizedBox(height: 20),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(labelText: 'Description'),
+            ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () => _addTask(context),
               child: Text('Add Task'),
             )
+
           ]
         )
       ) // Implement the form to add a task here

5ca5bb4cdf2382cc75a320988bc92876102d1b81
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-01-31 19:14:37 -0300

    Expand Task model with comprehensive task attributes

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,11 +1,27 @@
 class Task {
   final String id;
   final String title;
+  final String description;
+  final String creationDate;
+  final String priority;
+  final String status;
+  final String updatedAt;
+  final String dueDate;
   bool isDone;
+
+
 
   Task({
     required this.id,
     required this.title,
+    required this.description,
+    required this.creationDate,
+    required this.priority,
+    required this.status,
+    required this.updatedAt,
+    required this.dueDate,
     this.isDone = false,
   });
+
+
 }

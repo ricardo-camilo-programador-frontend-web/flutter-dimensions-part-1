535d5ee60c62e78ada4fddfc6a4949f3e619b9e5
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-01 23:35:22 -0300

    Add TaskProvider for centralized task state management

--- lib/logic/providers/task_provider.dart
+++ lib/logic/providers/task_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-import '../models/task.dart';
+import '../../data/models/task.dart';
 
 class TaskProvider with ChangeNotifier {
   List<Task> _tasks = [];
@@ -11,7 +11,7 @@ class TaskProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  void updateTaskStatus(Task task) {
+  void updateComplete(Task task) {
     _tasks[_tasks.indexOf(task)].isDone = !_tasks[_tasks.indexOf(task)].isDone;
     notifyListeners();
   }
@@ -22,10 +22,25 @@ class TaskProvider with ChangeNotifier {
     notifyListeners();
   }
 
+  void updateTaskPriority(Task task) {
+    final index = _tasks.indexOf(task);
+    
+    notifyListeners();
+  }
+
+
+  void updateTaskStatus(Task task) {
+    final index = _tasks.indexOf(task);
+
+    notifyListeners();
+  }
+
+
   void deleteTask(Task task) {
     _tasks.remove(task);
     notifyListeners();
   }
+
 
   // You can also add methods for fetching and managing tasks
 }
